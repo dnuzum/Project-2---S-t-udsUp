bbefaa14c2772994a22c6e72c8450f7a85b952e1
Derek Nuzum <derek.nuzum@gmail.com>
2016-02-20 14:56:30 -0800

    testing google search on heroku

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+// Home Page
+app.get("/", function(req, res) {
+	res.render("index");
+});
+
+
+
+// Google Search Auth
+app.get("/google51f837d0a25e2269.html", function(req, res) {
+	res.render("./google51f837d0a25e2269.html");
+});
 
 var port = process.env.PORT || 4000;
 app.listen(port, function() {
 	console.log("Running on " + port);
 });
+
